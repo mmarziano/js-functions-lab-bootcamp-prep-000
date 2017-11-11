f6ca23acfd397d9c402618968586b0b5cf36077c
Monica Marziano <mmarziano1@gmail.com>
2017-11-11 21:32:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code below!
+function happyHolidays() {
+  var greeting = "Happy holidays!";
+  return greeting;
+}
+
+function happyHolidaysTo ('name'){
+  name = 'Bella';
+  return `Happy holidays, $(name)!`
+
+}
