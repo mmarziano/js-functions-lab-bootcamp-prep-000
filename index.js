f164bd4295ec4f95caa25cc715bfa8ecaadb94d0
Monica Marziano <mmarziano1@gmail.com>
2017-11-11 21:38:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function happyHolidayTo(holiday, name){
   name = 'you';
   return `Happy ${holiday}, ${name}!`;
 }
+
+function holidayCountdown(days, holiday){
+  days = 5;
+  holiday = "Independence Day";
+  return `It's ${days} days until ${holiday}!`;
+}
